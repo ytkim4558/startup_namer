43b07d1c3e5d859f3184b162ed18e7c555662510
ytkim4558 <ytkim4558@naver.com>
2018-06-01 20:49:47 +0900

    테마를 화이트로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
 
     return new MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: new RandomWords(),
     );
   }
